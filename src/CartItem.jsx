3420a668fda9fdfea05a8fbe6557849db51b5b8b
Brasdor <marcel.cheng@hotmail.com>
2024-11-30 14:26:19 -0500

    task2 done

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,29 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + item.cost.slice(1) * item.quantity, 0).toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+  onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+    return (item.cost.slice(1) * item.quantity).toFixed(2);
   };
 
   // Calculate total cost based on quantity for an item
